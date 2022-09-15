f7283115e339cb25f3bf444012b6fb0e8b3dc646
OneDerry <olomu2001@gmail.com>
2022-09-15 13:34:57 +0100

    variable work 1

---
+++ html/jsfile/jsbasics.js
@@ -0,0 +1,40 @@
+// document.querySelectorAll('.btn').forEach( (item) => {
+//     item.addEventListener('click', () => {
+//         alert('this is awesome ')
+//     })
+// })
+
+// document.write('hello world')
+// alert('hello there')
+// console.log('hello people')
+
+// document.write('i am a new coder')
+// let name, address, zip, state;
+// name = "Mesiri";
+// address = "megattee junction eputu";
+// zip = "105101";
+// state = "Lagos";
+// name = "cashewnut"
+// console.log(name,address,zip,state);
+// console.log(name);
+
+// let fullName = "random"
+// console.log(fullName);
+
+//LET vs VAR vs CONST
+ 
+//using var
+// var value = "value";
+// console.log(value);
+
+// const name = "derry";
+// const lastname = "cls";
+// let fullName = name + ' ' + lastname;
+
+// console.log(fullName)
+
+// const website = "twitter";
+// const url = 'https://www.' + website + '.com';
+// console.log(url);
+
+
---
+++ html/jsfile/numbers.js
@@ -0,0 +1,2 @@
+const myName = 'chance'
+console.log(myName)
